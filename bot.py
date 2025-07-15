f5f25193eb930911d3a47f0a400a1e19c62dbc6c
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-15 12:57:14 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import os
-import asyncio
-import nest_asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
@@ -11,28 +9,19 @@ WEBHOOK_SECRET_KEY = os.getenv("WEBHOOK_SECRET_KEY")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await update.message.reply_text("Привет! Это NeoGPT, я запущен на Webhook с защитой!")
 
-async def main():
+def main():
   app = Application.builder().token(BOT_TOKEN).build()
   
 app.add_handler(CommandHandler("start", start))
 
-webhook_url = f"{BASE_URL}/webhook/{BOT_TOKEN}"
-
-# Установка webhook с секретным токеном
-await app.bot.set_webhook(
-  url=webhook_url,
-  secret_token=WEBHOOK_SECRET_KEY
-)
-
-print("Webhook установлен с секретом:", webhook_url)
-
-await app.run_webhook(
+# Запуск webhook-сервера с автоматическим set_webhook
+app.run_webhook(
   listen="0.0.0.0",
   port=8000,
   webhook_path=f"/webhook/{BOT_TOKEN}",
   secret_token=WEBHOOK_SECRET_KEY,
+  webhook_url=f"{BASE_URL}/webhook/{BOT_TOKEN}",
 )
 
 if __name__ == "__main__":
-  nest_asyncio.apply()
-  asyncio.run(main())
+ main()
